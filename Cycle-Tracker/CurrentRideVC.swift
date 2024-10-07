e02826292e8f89b317520bc64a905cec31cc88d7
Mesiow <wysockichris75@yahoo.com>
2024-10-07 14:22:08 -0400

    import core location and config buttons

--- Cycle-Tracker/CurrentRideVC.swift
+++ Cycle-Tracker/CurrentRideVC.swift
@@ -6,12 +6,50 @@
 //
 
 import UIKit
+import CoreLocation
 
 class CurrentRideVC: UIViewController {
 
+    let locManager = CLLocationManager();
+    var distanceTraveled = 0.0;
+    
+    var stopButton = CTButton(color: .systemRed, title: "Stop");
+    var pauseButton = CTButton(color: .systemYellow, title: "Pause");
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray6
+        
+        //request user permission to use location services while the app is in use
+        locManager.requestWhenInUseAuthorization();
+        locManager.delegate = self;
+        
+        configUI();
+    }
+    
+    func configUI(){
+        view.addSubview(stopButton);
+        view.addSubview(pauseButton);
+        
+        NSLayoutConstraint.activate([
+            stopButton.widthAnchor.constraint(equalToConstant: 160),
+            stopButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            stopButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25),
+        ])
+        
+        NSLayoutConstraint.activate([
+            pauseButton.widthAnchor.constraint(equalToConstant: 160),
+            pauseButton.centerYAnchor.constraint(equalTo: stopButton.centerYAnchor),
+            pauseButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -25),
+        ])
+    }
+}
+
+extension CurrentRideVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus){
+        if status == .authorizedWhenInUse {
+            print("Location Authorized")
+        }
     }
 }
